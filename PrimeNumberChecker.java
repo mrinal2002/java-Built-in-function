2c87edae479e691e91550e13bd5f4697a82bed48
Mrinal Patra <mrinal2002patra@gmail.com>
2025-03-08 09:12:53 +0530

    [Mrinal] desc: prime number checker

---
+++ PrimeNumberChecker.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class PrimeNumberChecker {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int num = scanner.nextInt();
+        
+        if (isPrime(num)) {
+            System.out.println(num + " is a prime number.");
+        } else {
+            System.out.println(num + " is not a prime number.");
+        }
+        
+        scanner.close();
+    }
+    
+    static boolean isPrime(int n) {
+        if (n < 2) return false;
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) return false;
+        }
+        return true;
+    }
+}
+
+/*
+Example Input/Output:
+Enter a number: 17
+17 is a prime number.
+
+Enter a number: 20
+20 is not a prime number.
+*/
